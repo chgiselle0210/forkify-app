cffe4556708d77a0ea6f9c40de3c0b9918ebb732
Giselle Cantú Chávez <0210giselle.ch@gmail.com>
2026-03-31 16:38:42 -0600

    Cambio en View.js para que se visualicen los íconos

--- js/views/View.js
+++ js/views/View.js
@@ -33,8 +33,10 @@ export default class View {
     const markup = `
       <div class="error">
         <div>
-          <svg>
-            <use href="${icons}#icon-alert-triangle"></use>
+          <svg viewBox="0 0 24 24" aria-hidden="true">
+            <path
+              d="M12 3L1 21h22L12 3zm0 5.5c.4 0 .7.3.7.7v5.1c0 .4-.3.7-.7.7s-.7-.3-.7-.7V9.2c0-.4.3-.7.7-.7zm0 9.3a1 1 0 110-2 1 1 0 010 2z"
+            ></path>
           </svg>
         </div>
         <p>${message}</p>
@@ -48,8 +50,10 @@ export default class View {
     const markup = `
       <div class="message">
         <div>
-          <svg>
-            <use href="${icons}#icon-smile"></use>
+          <svg viewBox="0 0 24 24" aria-hidden="true">
+            <path
+              d="M12 22C6.5 22 2 17.5 2 12S6.5 2 12 2s10 4.5 10 10-4.5 10-10 10zm-3.5-8a.75.75 0 00-1.06 1.06A6.2 6.2 0 0012 17c1.8 0 3.5-.7 4.8-1.94A.75.75 0 0015.74 14 4.7 4.7 0 0112 15.5c-1.4 0-2.7-.55-3.5-1.5zM9 10a1 1 0 110-2 1 1 0 010 2zm6 0a1 1 0 110-2 1 1 0 010 2z"
+            ></path>
           </svg>
         </div>
         <p>${message}</p>
